ae16d82e94b4ad3a1dae0f272e5973835818c71e
Fernando Martinez Ramirez <zS22013690@estudiantes.uv.mx>
2025-05-06 16:07:38 -0600

    Implementacion de token, falta ponerlo en las rutas

--- AuthService/app/utils/security.py
+++ AuthService/app/utils/security.py
@@ -1,4 +1,6 @@
 import bcrypt
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 import jwt
@@ -29,3 +31,16 @@ def decode_access_token(token: str) -> str | None:
         return payload.get("sub")
     except jwt.PyJWTError:
         return None
+    
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def verify_token(token: str = Depends(oauth2_scheme)) -> dict:
+    try:
+        payload = jwt.decode(token, settings.settings.JWT_SECRET_KEY, algorithms=[settings.settings.JWT_ALGORITHM])
+        return payload  # Devuelve el payload, que es el contenido del token
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expired")
+    except jwt.JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    return payload
