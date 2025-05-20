f1ed23347e4cfbd40f3d729ca8c11f385ef544cc
Fernando Martinez Ramirez <zS22013690@estudiantes.uv.mx>
2025-05-20 06:32:10 -0600

    Correcion de imagenes duplicadas

--- UserManagerService/app/utils/file_utils.py
+++ UserManagerService/app/utils/file_utils.py
@@ -1,12 +1,9 @@
 import os
 from urllib.parse import urlparse
 
-def delete_file_from_url(file_url: str):
-    try:
-        parsed = urlparse(file_url)
-        file_path = parsed.path.replace("/static", "static_user_content")
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            print(f"[INFO] Archivo eliminado: {file_path}")
-    except Exception as e:
-        print(f"[WARN] No se pudo eliminar archivo anterior: {e}")
+def delete_previous_files(directory: str, prefix: str):
+    for file in os.listdir(directory):
+        if file.startswith(prefix):
+            full_path = os.path.join(directory, file)
+            os.remove(full_path)
+            print(f"[INFO] Eliminado: {full_path}")
